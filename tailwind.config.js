5116cda4fcbb3a07e0de9881bea1532a07720a0a
jayeoni <jykchoi@ewhain.net>
2024-01-31 01:39:53 +0900

    brand color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +9,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "brand-1": "#60C91C",
+        "brand-2": "#F1F9EF",
+        "brand-3": "#FFAA00",
+        littleblack: "rgba(0, 0, 0, 0.5)",
+        gray: {
+          50: "#F9FAFB",
+          100: "#F2F4F6",
+          200: "#E5E8EB",
+          300: "#D1D6DB",
+          400: "#B0B8C1",
+          500: "#8D95A1",
+          600: "#6D7684",
+          700: "#4E5968",
+          800: "#333D4B",
+          900: "#191F28",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

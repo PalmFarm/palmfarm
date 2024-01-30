8af3322569a5a655507020645b63d815721029de
jayeoni <jykchoi@ewhain.net>
2024-01-30 18:51:16 +0900

    기본 설정 몇가지 수정

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <body id="app">
         <Main />
         <NextScript />
       </body>
